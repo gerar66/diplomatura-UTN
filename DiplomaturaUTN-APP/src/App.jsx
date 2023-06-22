32298aab1021d474f445b18368b90f198544a863
gerardo enriquez <enriquezgerardo66@gmail.com>
2023-06-22 18:09:15 -0300

    modulo 4 react

---
+++ DiplomaturaUTN-APP/src/App.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import "./App.css"
+
+export default function App() {
+  return (
+    <div>
+    <Box sx={{ flexGrow: 1 }} >
+      <AppBar position="absolute" >
+        <Toolbar >
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      
+    </Box>
+    <footer>
+    <p>Derechos de autor &copy; 2023 Transportes correcaminos SRL </p>
+  </footer>
+    </div>
+  );
+}
